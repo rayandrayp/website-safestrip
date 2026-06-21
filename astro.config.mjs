bd1697c149eb748a27057083d070fb7ea9505d5e
Rayandra Yala Pratama <rayandra.pratama@gmail.com>
2026-06-21 16:31:54 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/static';
 
 export default defineConfig({
   site: 'https://safestrip-three.vercel.app',
-  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
-  },
-  adapter: vercel({
-    webAnalytics: { enabled: true } // Aktifkan untuk memantau Core Web Vitals langsung di dashboard Vercel
-  }),
-  output: 'static',
+  }
 });
