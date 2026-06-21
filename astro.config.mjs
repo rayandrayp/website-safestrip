d7f8389a863e1cbe9bd1cc20524a827ae60c5210
Rayandra Yala Pratama <rayandra.pratama@gmail.com>
2026-06-21 12:41:12 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-// @ts-ignore: Ignore missing type declarations for Astro Tailwind integration
-import tailwind from '@astrojs/tailwind'; // Pastikan pakai integrasi resmi
+import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://safestrip-three.vercel.app',
-  integrations: [
-    tailwind(), // Menjalankan Tailwind
-    sitemap()   // Menjalankan Sitemap
-  ],
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
